614105a760e756976cefffe4cf15025804b531b9
RKV102 <roman.krilow@mail.ru>
2024-09-12 16:37:44 +0500

    Значения SECRET_KEY и ALLOWED_HOSTS теперь берутся из .env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s@ci&3$cv6%aw3!v)f&&(aob92qn_l8)ary39dum&q(3o0wqn!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
