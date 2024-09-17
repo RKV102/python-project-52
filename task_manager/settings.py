23ed97e661c739e7aa4f48d7a9b0263151066a5b
RKV102 <roman.krilow@mail.ru>
2024-09-18 01:00:24 +0500

    В список приложений добавлен django_extensions

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'task_manager',
     'task_manager.users',
     'django_bootstrap5',
