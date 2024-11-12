b1a84179dd18713477e781c6d69f866153b2379a
RKV102 <roman.krilow@mail.ru>
2024-11-12 23:16:08 +0500

    Рефакторинг LoginRequiredMixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,22 +1,26 @@
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import AccessMixin
 from django.contrib import messages
 from django.utils.translation import gettext as _
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import (LoginRequiredMixin as
+                                        DjangoLoginRequiredMixin)
 
 
-class LoginRequiredMixin(AccessMixin):
-    redirect_field_name = None
+class LoginRequiredMixin(DjangoLoginRequiredMixin):
     login_url = reverse_lazy('login')
 
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.error(
-                request,
-                _('You are not logged in! Please log in'),
-                extra_tags='danger'
-            )
-            return self.handle_no_permission()
+            return self.handle_no_permission(request)
         return super().dispatch(request, *args, **kwargs)
+
+    def handle_no_permission(self, request):
+        messages.error(
+            request,
+            _('You are not logged in! Please log in'),
+            extra_tags='danger'
+        )
+        return redirect(self.login_url)
 
 
 class BaseSuccessUrlMixin:
