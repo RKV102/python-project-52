fbc16516c453a98e07b48649b36c38d278ff7501
RKV102 <roman.krilow@mail.ru>
2024-11-15 21:20:12 +0500

    Рефакторинг тестов для users

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -26,42 +26,23 @@ class UserTestCase(TestCase):
             'password1': 'PsWd123*NeW',
             'password2': 'PsWd123*NeW'
         }
-        self.new_user_login_data = {
-            'username': 'test_username_new',
-            'password': 'PsWd123*NeW'
-        }
 
-    def create_user(self, create_data=None):
-        create_data = create_data or self.user_create_data
-        return self.client.post(
-            reverse('users_create'),
-            data=create_data,
-            follow=True
-        )
-
-    def login_user(self, login_data=None):
-        login_data = login_data or self.user_login_data
-        return self.client.post(
-            reverse('login'),
-            data=login_data,
-        )
-
-    def get_user_url(self, pk, url='users_update'):
-        return reverse(url, kwargs={'pk': pk})
-
-    def show_user(self, created_user):
-        return self.client.get(
-            self.get_user_url(created_user.pk),
-        )
-
-    def assert_user_data(self, response, username, first_name, last_name):
-        self.assertContains(response, username)
-        self.assertContains(response, first_name)
-        self.assertContains(response, last_name)
+    def assert_user_data(self, response, first_assert, second_assert,
+                         contains=True):
+        if contains:
+            self.assertContains(response, first_assert)
+            self.assertContains(response, second_assert)
+        else:
+            self.assertNotContains(response, first_assert)
+            self.assertNotContains(response, second_assert)
 
     def test_create_user(self):
         users_count = User.objects.count()
-        response = self.create_user()
+        response = self.client.post(
+            reverse('users_create'),
+            data=self.user_create_data,
+            follow=True
+        )
         self.assertEqual(User.objects.count(), users_count + 1)
         self.assertEqual(
             list(get_messages(response.wsgi_request))[-1].message,
@@ -69,23 +50,32 @@ class UserTestCase(TestCase):
         )
 
     def test_read_user(self):
-        self.create_user()
-        created_user = User.objects.last()
-        self.login_user()
-        response = self.show_user(created_user)
+        self.client.post(
+            reverse('users_create'),
+            data=self.user_create_data,
+            follow=True
+        )
+        response = self.client.get(reverse('users'))
         self.assert_user_data(
             response,
             self.user_create_data['username'],
-            self.user_create_data['first_name'],
-            self.user_create_data['last_name']
+            f"{self.user_create_data['first_name']} "
+            f"{self.user_create_data['last_name']}"
         )
 
     def test_update_user(self):
-        self.create_user()
+        self.client.post(
+            reverse('users_create'),
+            data=self.user_create_data,
+            follow=True
+        )
         created_user = User.objects.last()
-        self.login_user()
+        self.client.post(
+            reverse('login'),
+            data=self.user_login_data,
+        )
         response = self.client.post(
-            self.get_user_url(created_user.pk),
+            reverse('users_update', kwargs={'pk': created_user.pk}),
             data=self.user_update_data,
             follow=True
         )
@@ -93,30 +83,37 @@ class UserTestCase(TestCase):
             list(get_messages(response.wsgi_request))[-1].message,
             _("User has been updated")
         )
-        self.login_user(self.new_user_login_data)
-        response = self.show_user(created_user)
+        response = self.client.get(reverse('users'))
         self.assert_user_data(
             response,
             self.user_update_data['username'],
-            self.user_update_data['first_name'],
-            self.user_update_data['last_name']
+            f"{self.user_update_data['first_name']} "
+            f"{self.user_update_data['last_name']}"
         )
 
     def test_delete_user(self):
-        self.create_user()
+        self.client.post(
+            reverse('users_create'),
+            data=self.user_create_data,
+            follow=True
+        )
         created_user = User.objects.last()
-        self.login_user()
-        response = self.client.post(self.get_user_url(
-            created_user.pk,
-            'users_delete'
-        ))
+        self.client.post(
+            reverse('login'),
+            data=self.user_login_data,
+        )
+        response = self.client.post(
+            reverse('users_delete', kwargs={'pk': created_user.pk})
+        )
         self.assertEqual(
             list(get_messages(response.wsgi_request))[-1].message,
             _("User has been deleted")
         )
         response = self.client.get(reverse('users'))
-        self.assertNotContains(
+        self.assert_user_data(
             response,
+            self.user_create_data['username'],
             f"{self.user_create_data['first_name']} "
-            f"{self.user_create_data['last_name']}"
+            f"{self.user_create_data['last_name']}",
+            contains=False
         )
